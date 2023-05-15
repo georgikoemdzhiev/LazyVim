494e387ecdb3f43c81877d9ba2dbbd397688c30c
Georgi Koemdzhiev <koemdjiev@gmail.com>
2023-05-16 00:16:53 +0100

    Added Rust dap config

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,30 @@
+return {
+  "mfussenegger/nvim-dap",
+  config = function()
+    local dap = require("dap")
+    -- Rust/C
+    dap.adapters.lldb = {
+      type = "executable",
+      command = "/usr/bin/lldb-vscode", -- adjust as needed, must be absolute path
+      name = "lldb",
+    }
+    dap.configurations.cpp = {
+      {
+        name = "Launch",
+        type = "lldb",
+        request = "launch",
+        program = function()
+          return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+        end,
+        cwd = "${workspaceFolder}",
+        stopOnEntry = false,
+        args = {},
+      },
+    }
+
+    -- If you want to use this for Rust and C, add something like this:
+
+    dap.configurations.c = dap.configurations.cpp
+    dap.configurations.rust = dap.configurations.cpp
+  end,
+}
